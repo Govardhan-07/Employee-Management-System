b78da2241cdd19217e10b545ef33f3caaac6ed15
Govardhan <chgovardhanagiri7@gmail.com>
2023-09-09 18:06:35 +0530

    Final Commit

--- Employee-Management-System-Backend/src/main/java/com/codewithdev/employee/EmployeeManagementSystemBackendApplication.java
+++ Employee-Management-System-Backend/src/main/java/com/codewithdev/employee/EmployeeManagementSystemBackendApplication.java
@@ -1,4 +1,4 @@
-package com.codewithdev.Employee;
+package com.codewithdev.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Employee-Management-System-Backend/src/test/java/com/codewithdev/employee/EmployeeManagementSystemBackendApplicationTests.java
+++ Employee-Management-System-Backend/src/test/java/com/codewithdev/employee/EmployeeManagementSystemBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.codewithdev.Employee;
+package com.codewithdev.employee;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
